ad43cb109af8c26cf8dc0812fba4a75e6ce3833d
golap-raj <golapraj47@gmail.com>
2025-03-01 23:06:14 +0600

    add number

---
+++ js/text.js
@@ -0,0 +1,39 @@
+
+
+
+document.getElementById("first_button").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 5;
+    const retupNumber = document.getElementById("setUp_number").innerText = 24;
+      
+   
+})
+document.getElementById("first_button1").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 4;
+    const retupNumber = document.getElementById("setUp_number").innerText = 25;
+      
+   
+})
+document.getElementById("first_button2").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 3;
+    const retupNumber = document.getElementById("setUp_number").innerText = 26;
+      
+   
+})
+document.getElementById("first_button3").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 2;
+    const retupNumber = document.getElementById("setUp_number").innerText = 27;
+      
+   
+})
+document.getElementById("first_button4").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 1;
+    const retupNumber = document.getElementById("setUp_number").innerText = 28;
+      
+   
+})
+document.getElementById("first_button5").addEventListener("click", function(){
+    const renainTask = document.getElementById("remain_task").innerText = 6;
+    const retupNumber = document.getElementById("setUp_number").innerText = 29;
+      
+   
+})
