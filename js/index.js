6e7678f08eab6b24eb5bdd86d110daaaddcb81c1
golap-raj <golapraj47@gmail.com>
2025-03-01 23:34:38 +0600

    add diseble button

--- js/index.js
+++ js/index.js
@@ -22,3 +22,51 @@ function generateRandomColor(){
 
 }
 
+
+
+// button diseble and change number
+
+
+
+document.getElementById("first_button").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 5;
+    const retupNumber = document.getElementById("setUp_number").innerText = 24;
+      
+   
+})
+document.getElementById("first_button1").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 4;
+    const retupNumber = document.getElementById("setUp_number").innerText = 25;
+      
+   
+})
+document.getElementById("first_button2").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 3;
+    const retupNumber = document.getElementById("setUp_number").innerText = 26;
+      
+   
+})
+document.getElementById("first_button3").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 2;
+    const retupNumber = document.getElementById("setUp_number").innerText = 27;
+      
+   
+})
+document.getElementById("first_button4").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 1;
+    const retupNumber = document.getElementById("setUp_number").innerText = 28;
+      
+   
+})
+document.getElementById("first_button5").addEventListener("click", function(event){
+    event.target.disabled = true;
+    const renainTask = document.getElementById("remain_task").innerText = 6;
+    const retupNumber = document.getElementById("setUp_number").innerText = 29;
+      
+   
+})
